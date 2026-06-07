5211a3075696d3016662f842d232e33da15c5200
Poof <andreypavelko27@gmail.com>
2026-06-07 13:59:02 +0300

    HW12

---
+++ src/main.js
@@ -0,0 +1,61 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+  showLoadMoreButton,
+  hideLoadMoreButton,
+} from './render-functions';
+
+const form = document.querySelector('.form');
+const loadMoreBtn = document.querySelector('.load-more');
+
+let page = 1;
+let query = '';
+let totalHits = 0;
+
+form.addEventListener('submit', onSubmit);
+loadMoreBtn.addEventListener('click', onLoadMore);
+
+async function onSubmit(event) {
+  event.preventDefault();
+
+  query = event.currentTarget.elements.searchText.value.trim();
+
+  if (!query) return;
+
+  page = 1;
+
+  clearGallery();
+  hideLoadMoreButton();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query, page);
+
+    totalHits = data.totalHits;
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+
+      return;
+    }
+
+    createGallery(data.hits);
+
+    if (totalHits > 15) {
+      showLoadMoreButton();
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    hideLoader();
+  }
+}
